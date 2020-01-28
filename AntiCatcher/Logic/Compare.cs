4d16cab2a21f505781e151b7c936e0b1baf8f56f
Yaroslav Afanasiev <yarmylo@yandex.ru>
2020-01-28 23:26:47 +0300

    Add Compare and UserInfo

---
+++ AntiCatcher/Logic/Compare.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AntiCatcher.Logic
+{
+  public class Compare
+    {
+        public int CompareUsr(string CarSign, UsrData.UsrInfo UsrBd)
+        {
+            string CarSigner = CarSign;
+
+            foreach (var CarSigner in UsrBd)
+            {
+
+            }
+        }
+    }
+}
--- AntiCatcher/Program.cs
+++ AntiCatcher/Program.cs
@@ -4,12 +4,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+
 namespace AntiCatcher
 {
     class Program
     {
         static void Main(string[] args)
         {
+            UsrData.UsrInfo usr1 = new UsrData.UsrInfo();
+
+            #region userInit
+            usr1.FirstName = ("Andrey");
+            usr1.LastName = ("Abramov");
+            usr1.CarSign = ("а456кк777");
+            usr1.PhoneNumber = ("+79314567432");
+            #endregion
+
+            List<UsrData.UsrInfo> UsrBd = new List<UsrData.UsrInfo>() { };
+
+            UsrBd.Add(usr1);
+
+            string comparison = Console.ReadLine();
+
+            Logic.Compare comp1 = new Logic.Compare();
+
+            comp1.CompareUsr(comparison, UsrBd);
+
+            
+            Console.ReadLine();
+            
         }
     }
 }
